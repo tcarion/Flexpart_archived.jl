b95ed154321196a80aa983f7835a96c7488cca2d
tcarion <tristancarion@gmail.com>
2022-05-05 12:16:28 +0000

    reduce number test run

--- test/run.jl
+++ test/run.jl
@@ -3,10 +3,4 @@ using Flexpart
 using Test
 using Dates
 
-tmpdir = FlexpartDir()
-Flexpart.run(tmpdir)
-Flexpart.run(tmpdir, log = true)
-
-@test isfile(joinpath(tmpdir[:output], "output.log"))
-
 default_run(tmpdir)
